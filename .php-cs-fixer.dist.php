68199e5737f3457dae76342e9310fd5ad58764ef
alvin371 <alvindimas8@gmail.com>
2026-06-19 10:27:07 +0700

    fix(ci): include test assets in docker build context

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -4,14 +4,16 @@ use CodeIgniter\CodingStandard\CodeIgniter4;
 use Nexus\CsConfig\Factory;
 use PhpCsFixer\Finder;
 
+$paths = array_filter(array(
+    __DIR__ . '/application',
+    __DIR__ . '/migrations',
+    __DIR__ . '/tests',
+    __DIR__ . '/tools',
+), 'is_dir');
+
 $finder = Finder::create()
     ->files()
-    ->in(array(
-        __DIR__ . '/application',
-        __DIR__ . '/migrations',
-        __DIR__ . '/tests',
-        __DIR__ . '/tools',
-    ))
+    ->in($paths)
     ->append(array(
         __DIR__ . '/index.php',
     ))
